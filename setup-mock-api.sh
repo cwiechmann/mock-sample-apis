16d7d2fbb0587fad9e543ee19ba35cfef86c248e
Chris Wiechmann <cwiechmann@axway.com>
2020-07-09 22:42:31 +0200

    Create setup-mock-api.sh

---
+++ setup-mock-api.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p apim-cli
+
+sudo curl -L https://github.com/Axway-API-Management-Plus/apim-cli/releases/download/apimcli-1.2.0-RC1/axway-apimcli-1.2.0-SNAPSHOT.tar.gz | sudo tar xvfz
+
+export JAVA_HOME=/home/ec2-user/Axway-7.7.0/apigateway/Linux.x86_64/jre
+
+./apim-cli/apim-cli-1.2.0-SNAPSHOT/scripts/apim.sh api import -h localhost -u apiadmin -p changeme -c ../apim-cli-data/petstore-v2/api-config.json
