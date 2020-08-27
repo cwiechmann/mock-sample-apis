63e3f20708569f6683c20e3e9e0dd50ff16ed353
Chris Wiechmann <cwiechmann@axway.com>
2020-08-27 20:22:56 +0200

    Make life easier

--- setup-mock-api.sh
+++ setup-mock-api.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-mkdir -p apim-cli
 
-wget -qO- https://github.com/Axway-API-Management-Plus/apim-cli/releases/download/apimcli-1.2.0-RC1/axway-apimcli-1.2.0-SNAPSHOT.tar.gz | tar xvfz  - -C apim-cli
+if [ ! -d "$DIR" ]; then
+    mkdir -p apim-cli
+    wget -qO- https://github.com/Axway-API-Management-Plus/apim-cli/releases/download/apimcli-1.2.0-RC1/axway-apimcli-1.2.1.tar.gz | tar xvfz  - -C apim-cli
+fi
 
 export JAVA_HOME=/home/ec2-user/Axway-7.7.0/apigateway/Linux.x86_64/jre
 
-./apim-cli/apim-cli-1.2.0-SNAPSHOT/scripts/apim.sh app import -h localhost -u apiadmin -p changeme -c $PWD/apim-cli-data/Client-App/application-config.json
+./apim-cli/apim-cli-1.2.1/scripts/apim.sh app import -h localhost -u apiadmin -p changeme -c $PWD/apim-cli-data/Client-App/application-config.json
 
-./apim-cli/apim-cli-1.2.0-SNAPSHOT/scripts/apim.sh api import -h localhost -u apiadmin -p changeme -c $PWD/apim-cli-data/petstore-v2/api-config.json
+./apim-cli/apim-cli-1.2.1/scripts/apim.sh api import -h localhost -u apiadmin -p changeme -c $PWD/apim-cli-data/petstore-v2/api-config.json
