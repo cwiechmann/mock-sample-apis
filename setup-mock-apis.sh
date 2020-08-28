6840926edc3d25d67e4236dc99f19ef3290d3cb0
EC2 Default User <ec2-user@ip-172-31-48-17.ec2.internal>
2020-08-28 07:59:07 +0000

    Setup script tested/fixed

--- setup-mock-apis.sh
+++ setup-mock-apis.sh
@@ -1,32 +1,41 @@
 #!/bin/sh
 
+currentDir=$PWD
+APIM_CLI_VERSION=1.2.1
+CLI_DIR=$currentDir/apim-cli
 
-if [ ! -d "$DIR" ]; then
-    mkdir -p apim-cli
-    wget -qO- https://github.com/Axway-API-Management-Plus/apim-cli/releases/download/apimcli-1.2.1/axway-apimcli-1.2.1.tar.gz | tar xvfz  - -C apim-cli
+if [ ! -d "$CLI_DIR/apim-cli-$APIM_CLI_VERSION" ]; then
+    mkdir $CLI_DIR
+    wget -qO- https://github.com/Axway-API-Management-Plus/apim-cli/releases/download/apimcli-$APIM_CLI_VERSION/axway-apimcli-$APIM_CLI_VERSION.tar.gz | tar xvfz  - -C $CLI_DIR
 fi
+
+CLI=$CLI_DIR/apim-cli-$APIM_CLI_VERSION/scripts/apim.sh
 
 export JAVA_HOME=/home/ec2-user/Axway-7.7.0/apigateway/Linux.x86_64/jre
 
-currentDir=$PWD
 
 # Import all organizations
 cd $currentDir/apim-cli-data/Organizations
 for org in *.json
 do
-    ./apim-cli/apim-cli-1.2.1/scripts/apim.sh org import -h localhost -u apiadmin -p changeme -c $org
+    echo "Import organization from config: $org"
+    $CLI org import -h localhost -u apiadmin -p changeme -c $currentDir/apim-cli-data/Organizations/$org
 done
 
 # Import all applications
 cd $currentDir/apim-cli-data/ClientApps
-for appDirectory in *.json
+for appDirectory in `find . -type d`
 do
-    ./apim-cli/apim-cli-1.2.1/scripts/apim.sh app import -h localhost -u apiadmin -p changeme -c $appDirectory/application-config.json
+    echo "Import applicaton from config directory: $appDirectory"
+    $CLI app import -h localhost -u apiadmin -p changeme -c $currentDir/apim-cli-data/ClientApps/$appDirectory/application-config.json
 done
 
 # Import all APIs
 cd $currentDir/apim-cli-data/APIs
-for apiDirectory in *.json
+for apiDirectory in `find . -type d`
 do
-    ./apim-cli/apim-cli-1.2.1/scripts/apim.sh org import -h localhost -u apiadmin -p changeme -c $apiDirectory/api-config.json
+    echo "Import API from config directory: $apiDirectory"
+    $CLI api import -h localhost -u apiadmin -p changeme -c $currentDir/apim-cli-data/APIs/$apiDirectory/api-config.json
 done
+
+exit
