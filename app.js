b0c5f63efe3fa7d28c5dc62eb1527ee14cc2cf84
Chris Wiechmann <cwiechmann@axway.com>
2021-12-07 10:49:35 +0100

    Added APM-Server

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const APIBuilder = require('@axway/api-builder-runtime');
+
+if(process.env.APM_ENABLED) {
+	console.log(`Application performance monitoring enabled. Using APM-Server: ${process.env.APM_SERVER || 'https://axway-elk-apm-server:8200'}`);
+	require('elastic-apm-node').start({
+		serviceName: 'MockedDemoAPIs',
+		serverUrl: process.env.APM_SERVER || 'https://axway-elk-apm-server:8200', 
+		verifyServerCert: false, 
+		serverCaCertFile: process.env.APM_SERVER_CA || 'config/certificates/ca.crt'
+	});
+}
+
 const server = new APIBuilder();
 
 // lifecycle examples
